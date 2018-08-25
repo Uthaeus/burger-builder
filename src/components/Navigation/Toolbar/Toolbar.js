2925b012425c7dcdc74630b61ee95c0477efb872
Uthaeus <romanlavery@gmail.com>
2018-08-24 22:06:22 -0600

    Added a toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <div>LOGO</div>
+    <nav>
+      ...
+    </nav>
+  </header>
+);
+
+
+export default toolbar;
